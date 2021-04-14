79e4ced8de9c7768dee5d82b9bd5cf75311135dc
gsadanala <gsadanala@paypal.com>
2021-04-14 15:53:46 +0530

    create the browser app

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+var app = express();
+app.use(express.static(__dirname));
+var server = app.listen(3000, () => {
+  console.log(`server is listening on port ${server.address().port}`);
+});
+
+console.log(server.address());
