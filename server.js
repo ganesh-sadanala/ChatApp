0784c9328162f13ac8b3d629c72bf4d98d09a651
gsadanala <gsadanala@paypal.com>
2021-04-15 12:07:39 +0530

    Create a get messages service

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 var app = express();
 app.use(express.static(__dirname));
+
+var messages = [
+  { name: "Time", message: "Hi" },
+  { name: "Bim", message: "Hi" },
+];
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
 var server = app.listen(3000, () => {
   console.log(`server is listening on port ${server.address().port}`);
 });
