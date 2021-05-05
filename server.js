df82d488392bc370d8a041b21eb2b21c78a9fd0e
gsadanala <gsadanala@paypal.com>
2021-05-05 15:49:05 +0530

    connect to mongodb using mongoose

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 var app = express();
 var http = require("http").Server(app);
@@ -6,12 +7,15 @@ app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 var messages = [];
+var mongoose = require("mongoose");
 
+var dbUrl = `mongodb+srv://${process.env.username}:${process.env.password}@cluster0.mdhsd.mongodb.net/chat_db?retryWrites=true&w=majority`;
+console.log(dbUrl);
 app.get("/messages", (req, res) => {
   res.send(messages);
 });
 
-app.post("/messages", (req, res) => {
+https: app.post("/messages", (req, res) => {
   messages.push(req.body);
   io.emit("message", req.body);
   res.sendStatus(200);
@@ -21,6 +25,9 @@ io.on("connection", (socket) => {
   console.log("user is connected");
 });
 
+mongoose.connect(dbUrl, (err) => {
+  console.error(err);
+});
 var server = http.listen(3000, () => {
   console.log(`server is listening on port ${server.address().port}`);
 });
