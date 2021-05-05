a80f4642d7fac7b9a8f6db8e17261c95f7cc7230
gsadanala <gsadanala@paypal.com>
2021-05-05 11:57:40 +0530

    connect to socket.io from the browser app

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,8 +17,10 @@ app.post("/messages", (req, res) => {
   res.sendStatus(200);
 });
 
-var server = app.listen(3000, () => {
-  console.log(`server is listening on port ${server.address().port}`);
+io.on("connection", (socket) => {
+  console.log("user is connected");
 });
 
-console.log(server.address());
+var server = http.listen(3000, () => {
+  console.log(`server is listening on port ${server.address().port}`);
+});
